03716835c42f83c0f7334253a9fa45768bd698ba
imran94 <iraja094@gmail.com>
2016-06-30 00:29:54 +0800

    Add discovery ability

--- app/src/main/java/com/project/imran/devicediscovery/MainActivity.java
+++ app/src/main/java/com/project/imran/devicediscovery/MainActivity.java
@@ -1,5 +1,7 @@
 package com.project.imran.devicediscovery;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,6 +16,7 @@ import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements
 
     private boolean mIsHost = false;
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG = "sup";
 
     private static final long TIMEOUT_ADVERTISE = 1000L * 30L;
     private static final long TIMEOUT_DISCOVER = 1000L * 30L;
@@ -72,11 +75,17 @@ public class MainActivity extends AppCompatActivity implements
         findViewById(R.id.button_discover).setOnClickListener(this);
         findViewById(R.id.button_send).setOnClickListener(this);
 
+        // Debug text view
+        mDebugInfo = (TextView) findViewById(R.id.debug_text);
+//        mDebugInfo.setMovementMethod(new ScrollingMovementMethod());
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(Nearby.CONNECTIONS_API)
                 .build();
+
+        updateViewVisiblity(STATE_READY);
     }
 
     @Override
@@ -93,6 +102,11 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
     public void onConnectionSuspended(int i) {
         debugLog("onConnectionSuspended: " + i);
     }
@@ -100,8 +114,8 @@ public class MainActivity extends AppCompatActivity implements
     private boolean isConnectedToNetwork() {
         ConnectivityManager connManager =
                 (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo info = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        return info != null && info.isConnectedOrConnecting();
+        NetworkInfo info = connManager.getActiveNetworkInfo();
+        return info != null && info.isConnected();
     }
 
     private void startAdvertising() {
@@ -126,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements
                     public void onResult(Connections.StartAdvertisingResult result) {
                         if (result.getStatus().isSuccess()) {
                             debugLog("startAdvertising:onResult: SUCCESS");
-                            // Update view visibility
+                            updateViewVisiblity(STATE_ADVERTISING);
                         } else {
                             debugLog("startAdvertising:onResult: FAILURE ");
 
@@ -134,7 +148,38 @@ public class MainActivity extends AppCompatActivity implements
                             if (statusCode == ConnectionsStatusCodes.STATUS_ALREADY_ADVERTISING){
                                 debugLog("Already advertising");
                             } else {
-                                // Update View Visibility
+                                updateViewVisiblity(STATE_READY);
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void startDiscovery() {
+        if (!isConnectedToNetwork()) {
+            debugLog("startAdvertising: Not connected to network");
+            return;
+        }
+
+        String serviceId = getString(R.string.service_id);
+
+        long DISCOVER_TIMEOUT = 1000L;
+
+        Nearby.Connections.startDiscovery(mGoogleApiClient, serviceId, DISCOVER_TIMEOUT, this)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            debugLog("startDiscovery:onResult: SUCCESS");
+                            updateViewVisiblity(STATE_DISCOVERING);
+                        } else {
+                            debugLog("startAdvertising:onResult: FAILURE ");
+
+                            int statusCode = status.getStatusCode();
+                            if (statusCode == ConnectionsStatusCodes.STATUS_ALREADY_DISCOVERING){
+                                debugLog("Already discovering");
+                            } else {
+                                updateViewVisiblity(STATE_READY);
                             }
                         }
                     }
@@ -147,7 +192,7 @@ public class MainActivity extends AppCompatActivity implements
                 startAdvertising();
                 break;
             case R.id.button_discover:
-                // startDiscovery();
+                 startDiscovery();
                 break;
             case R.id.button_send:
                 // sendMessage();
@@ -155,8 +200,61 @@ public class MainActivity extends AppCompatActivity implements
         }
     }
 
+    private void updateViewVisiblity(int newState) {
+        mState = newState;
+
+        switch(mState) {
+            case STATE_IDLE:
+                findViewById(R.id.layout_nearby_buttons).setVisibility(View.GONE);
+                findViewById(R.id.layout_message).setVisibility(View.GONE);
+                break;
+            case STATE_READY:
+                findViewById(R.id.layout_nearby_buttons).setVisibility(View.VISIBLE);
+                findViewById(R.id.layout_message).setVisibility(View.GONE);
+                break;
+            case STATE_DISCOVERING:
+                break;
+            case STATE_ADVERTISING:
+                break;
+            case STATE_CONNECTED:
+                findViewById(R.id.layout_nearby_buttons).setVisibility(View.VISIBLE);
+                findViewById(R.id.layout_message).setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
     private void debugLog(String msg) {
         Log.d(TAG, msg);
         mDebugInfo.append("\n" + msg);
+    }
+
+    @Override
+    public void onConnectionRequest(String s, String s1, String s2, byte[] bytes) {
+
+    }
+
+    @Override
+    public void onEndpointFound(String s, String s1, String s2, String s3) {
+
+    }
+
+    @Override
+    public void onEndpointLost(String s) {
+
+    }
+
+    @Override
+    public void onMessageReceived(String s, byte[] bytes, boolean b) {
+
+    }
+
+    @Override
+    public void onDisconnected(String s) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
