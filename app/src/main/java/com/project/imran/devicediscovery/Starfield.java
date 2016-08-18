5a0e1734e644142d0d2b3e4f3a8bdd7f3fa113f7
imran94 <iraja094@gmail.com>
2016-08-19 01:49:46 +0800

    Load images and make them scroll in Starfield

--- app/src/main/java/com/project/imran/devicediscovery/Starfield.java
+++ app/src/main/java/com/project/imran/devicediscovery/Starfield.java
@@ -1,7 +1,13 @@
 package com.project.imran.devicediscovery;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -35,10 +41,13 @@ public class Starfield {
             "precision mediump float;" +
                     "uniform vec4 vColor;" +
                     "uniform sampler2D TexCoordIn;" +
+                    "uniform float scroll;" +
                     "varying vec2 TexCoordOut;" +
                     "void main() {" +
-                    " gl_FragColor = texture2D(TexCoordIn, vec2(TexCoordOut.x,TexCoordOut.y));" +
-            "}";
+                        " gl_FragColor = texture2D(TexCoordIn, " +
+                        /*Scrolls along y-axis */
+                        " vec2(TexCoordOut.x ,TexCoordOut.y + scroll));" +
+                    "}";
 
     private float texture[] = {
             -1f, 1f,
@@ -47,14 +56,20 @@ public class Starfield {
             1f, 1f,
     };
 
+    private int textures[] = new int[1];
     private final FloatBuffer vertexBuffer;
     private final ShortBuffer drawListBuffer;
+    private final FloatBuffer textureBuffer;
     private final int mProgram;
+
     private int mPositionHandle;
     private int mColorHandle;
     private int mMVPMatrixHandle;
+
     static final int COORDS_PER_VERTEX = 3;
+    static final int COORDS_PER_TEXTURE = 2;
     private final int vertexStride = COORDS_PER_VERTEX * 4;
+    public static int textureStride = COORDS_PER_TEXTURE * 4;
 
     public Starfield() {
         ByteBuffer bb = ByteBuffer.allocateDirect(squareCoords.length * 4);
@@ -62,11 +77,19 @@ public class Starfield {
         vertexBuffer = bb.asFloatBuffer();
         vertexBuffer.put(squareCoords);
         vertexBuffer.position(0);
+
+        bb = ByteBuffer.allocateDirect(texture.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        textureBuffer = bb.asFloatBuffer();
+        textureBuffer.put(texture);
+        textureBuffer.position(0);
+
         ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
         dlb.order(ByteOrder.nativeOrder());
         drawListBuffer = dlb.asShortBuffer();
         drawListBuffer.put(drawOrder);
         drawListBuffer.position(0);
+
         int vertexShader = GameRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
                 vertexShaderCode);
         int fragmentShader = GameRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
@@ -77,20 +100,82 @@ public class Starfield {
         GLES20.glLinkProgram(mProgram);
     }
 
-    public void draw(float[] mvpMatrix) {
+    public void draw(float[] mvpMatrix, float scroll) {
         GLES20.glUseProgram(mProgram);
+
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
         GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        int vsTextureCoord = GLES20.glGetAttribLocation(mProgram, "TextCoordIn");
+
         GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
                 GLES20.GL_FLOAT, false,
                 vertexStride, vertexBuffer);
-        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram,
-                "uMVPMatrix");
+
+        GLES20.glVertexAttribPointer(vsTextureCoord, COORDS_PER_TEXTURE,
+                GLES20.GL_FLOAT, false,
+                textureStride, textureBuffer);
+
+        GLES20.glEnableVertexAttribArray(vsTextureCoord);
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0]);
+
+        // Access texture coordinatees from shader and assign it to 0
+        int fsTexture = GLES20.glGetUniformLocation(mProgram, "TexCoordOut");
+        GLES20.glUniform1i(fsTexture, 0);
+
+        // Access scroll variable from shader
+        int fsScroll = GLES20.glGetUniformLocation(mProgram, "scroll");
+        // Assign it a new value
+        GLES20.glUniform1f(fsScroll, scroll);
+
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
         GameRenderer.checkGLError("glGetUniformLocation");
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
         GameRenderer.checkGLError("glUniformMatrix4fv");
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length,
                 GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
         GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
+
+    public void loadTexture(int texture, Context context) {
+        InputStream imageStream = context.getResources().
+                                    openRawResource(texture);
+        Bitmap bitmap = null;
+
+        android.graphics.Matrix flip = new android.graphics.Matrix();
+        flip.postScale(-1f, -1f);
+
+        try {
+
+            bitmap = BitmapFactory.decodeStream(imageStream);
+
+        } catch(Exception e) {
+
+        }finally {
+            try {
+                imageStream.close();
+                imageStream = null;
+            } catch (IOException io) {
+
+            }
+        }
+
+        GLES20.glGenTextures(1, textures, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0]);
+
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_REPEAT);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_REPEAT);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+        bitmap.recycle();
     }
 }
